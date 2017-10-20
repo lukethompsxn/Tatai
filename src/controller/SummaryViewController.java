a52b066195a88165bb7b80c5de55e1c5fd3fc0ef
joelz1 <joel.clarke@live.com>
2017-10-20 13:42:16 +1300

    Stats stuff added

--- src/controller/SummaryViewController.java
+++ src/controller/SummaryViewController.java
@@ -5,6 +5,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import model.ModeDirector;
+import model.Statistics;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -17,6 +18,7 @@ public class SummaryViewController extends AbstractController implements Initial
     private Label scoreLbl;
 
     private static ModeDirector _modeDirector = ModeDirector.instance();
+    private static Statistics _statistics = Statistics.instance();
 
     @FXML
     private void returnBtnAction() {
@@ -27,6 +29,35 @@ public class SummaryViewController extends AbstractController implements Initial
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        scoreLbl.setText("You scored " + _modeDirector.getScore() + "/" + _modeDirector.getNumQuestions());
+        int score = _modeDirector.getScore();
+        scoreLbl.setText("You scored " + score + "/" + _modeDirector.getNumQuestions());
+        _statistics.readFileRecentScores();
+
+        if (_modeDirector.getMode() == ModeDirector.Mode.MATH_ADD) {
+            if (score > _statistics.get_highScoreAdd()) {
+                _statistics.set_highScoreAdd(score);
+            }
+            _statistics.addRecentScoresAdd(score);
+        } else if (_modeDirector.getMode() == ModeDirector.Mode.MATH_SUB) {
+            if (score > _statistics.get_highScoreSub()) {
+                _statistics.set_highScoreSub(score);
+            }
+            _statistics.addRecentScoresSub(score);
+        } else if (_modeDirector.getMode() == ModeDirector.Mode.MATH_DIV) {
+            if (score > _statistics.get_highScoreDiv()) {
+                _statistics.set_highScoreDiv(score);
+            }
+            _statistics.addRecentScoresDiv(score);
+        } else if (_modeDirector.getMode() == ModeDirector.Mode.MATH_MULT) {
+            if (score > _statistics.get_highScoreMult()) {
+                _statistics.set_highScoreMult(score);
+            }
+            _statistics.addRecentScoresMult(score);
+        }
+        _statistics.setQuestionsAnswered(_statistics.getQuestionsAnswered() + _modeDirector.getNumQuestions());
+        _statistics.setQuizzesCompleted(_statistics.getQuizzesCompleted() + 1);
+        _statistics.writeToFileHighScores();
+        _statistics.writeToFileRecentScores();
+
     }
 }
