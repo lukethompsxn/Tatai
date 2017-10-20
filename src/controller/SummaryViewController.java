7796b322428a7fe47c8d90c4a2023543f9aa790f
Luke Thompson <lukethompsxn@gmail.com>
2017-10-20 23:29:18 +1300

    summary view

--- src/controller/SummaryViewController.java
+++ src/controller/SummaryViewController.java
@@ -1,11 +1,15 @@
 package controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import model.ModeDirector;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -25,8 +29,16 @@ public class SummaryViewController extends AbstractController implements Initial
         popChild();
     }
 
+    public void mainMenu() {
+        try {
+            pushPopup(new Scene(FXMLLoader.load(getClass().getResource(File.separator + "view" + File.separator + "AreYouSurePopup.fxml"))), true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        scoreLbl.setText("You scored " + _modeDirector.getScore() + "/" + _modeDirector.getNumQuestions());
+        scoreLbl.setText(_modeDirector.getScore() + "/" + _modeDirector.getNumQuestions());
     }
 }
